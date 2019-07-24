4713b86bb8215074be82abea020d9e64b7c7c17c
Greg <fredianig11@gmail.com>
2019-07-25 00:28:45 +0200

    Cleaned up JS code and added comments. Created stylesheet.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,24 @@
-//Keeps record of player's & computer's score
+//Variables for stats
 let playerScore = 0
 let computerScore = 0
-let round = 0
+let round = 1
+
+//Variables for DOM Methods
+let currentRoundDisplay = document.getElementById('roundDisplay')
+let playerChoiceDisplay = document.getElementById('playerChoice')
+let computerChoiceDisplay = document.getElementById('computerChoice')
+let roundDisplay = document.getElementById('roundResults');
+let resultsDisplay = document.getElementById('results')
+let playerScoreDisplay = document.getElementById('playerScore')
+let computerScoreDisplay = document.getElementById('computerScore')
+let rockBtn = document.getElementById('rock')
+let paperBtn = document.getElementById('paper')
+let scissorsBtn = document.getElementById('scissors')
+
+//Event listener's for buttons
+rockBtn.addEventListener('click', () => game('rock'))
+paperBtn.addEventListener('click', () => game('paper'))
+scissorsBtn.addEventListener('click', () => game('scissors'))
 
 //Computer's selection
 function computerPlay(){
@@ -14,13 +31,15 @@ function computerPlay(){
             return 'scissors';
     };
 }
-//plays the game for 5 rounds and keeps score
+
+//plays the game
 function game(playerSelection){
     let computerSelection = computerPlay();
     playRound(playerSelection,computerSelection)
     displayMatchResults(computerScore, playerScore)
 }
 
+//Displays results after player or computer reaches 5 points
 function displayMatchResults(computerScore, playerScore){
     let playerWinScore = playerScore - computerScore
     let playerLossScore = computerScore - playerScore
@@ -34,55 +53,30 @@ function displayMatchResults(computerScore, playerScore){
     }
 }
 
-//Plays a round and returns the score
+//Logic for playing a round
 function playRound(playerSelection, computerSelection){
     let playerSelectionCapitalized = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
     let computerSelectionCapitalized = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
     
     if (playerSelection == computerSelection){
-        round++
-        playerChoiceDisplay.textContent = 'Player chose: ' + playerSelectionCapitalized
-        computerChoiceDisplay.textContent = 'Computer chose: ' + computerSelectionCapitalized
         roundDisplay.innerText = 'Draw! No points awarded.'
         currentRoundDisplay.textContent = `Round: ${round}`
     } else if (playerSelection == 'rock' && computerSelection == 'scissors' || 
     playerSelection == 'paper' && computerSelection == 'rock' || 
     playerSelection == 'scissors' && computerSelection == 'paper'){
         playerScore++;
-        round++;
         currentRoundDisplay.textContent = `Round: ${round}`
         playerScoreDisplay.textContent = `Player score: ${playerScore}`;
-        playerChoiceDisplay.textContent = 'Player chose: ' + playerSelectionCapitalized
-        computerChoiceDisplay.textContent = 'Computer chose: ' + computerSelectionCapitalized
         roundDisplay.textContent =  playerSelectionCapitalized + ' beats ' +
         computerSelection + '! One point to player!'
-
     } else {
         computerScore++;
-        round++;
         currentRoundDisplay.textContent = `Round: ${round}`
         computerScoreDisplay.textContent = `Computer score: ${computerScore}`
-        playerChoiceDisplay.textContent = 'Player chose: ' + playerSelectionCapitalized
-        computerChoiceDisplay.textContent = 'Computer chose: ' + computerSelectionCapitalized
         roundDisplay.textContent =  computerSelectionCapitalized + ' beats ' +
         playerSelection + '! One point to computer!'
     }
+    playerChoiceDisplay.textContent = 'Player chose: ' + playerSelectionCapitalized
+    computerChoiceDisplay.textContent = 'Computer chose: ' + computerSelectionCapitalized
+    round++;
 }
-
-let currentRoundDisplay = document.getElementById('roundDisplay')
-let playerChoiceDisplay = document.getElementById('playerChoice')
-let computerChoiceDisplay = document.getElementById('computerChoice')
-let roundDisplay = document.getElementById('roundResults');
-let resultsDisplay = document.getElementById('results')
-let playerScoreDisplay = document.getElementById('playerScore')
-let computerScoreDisplay = document.getElementById('computerScore')
-
-let rockBtn = document.getElementById('rock')
-let paperBtn = document.getElementById('paper')
-let scissorsBtn = document.getElementById('scissors')
-
-rockBtn.addEventListener('click', () => game('rock'))
-paperBtn.addEventListener('click', () => game('paper'))
-scissorsBtn.addEventListener('click', () => game('scissors'))
-
-//plays the game
